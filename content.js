910fd35873e1b5f88f7f5166cb1e557391271857
tsleeve <akash5malik@gmail.com>
2015-09-11 02:06:46 -0700

    add button

--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-alert("This guy is a faggot");
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse){
+		if(request.message === "clicked_browser_action")
+		{
+			var firstHref = $("a[href^='http']").eq(0).attr("href");
+			alert(firstHref);
+		}
+	}
+);
