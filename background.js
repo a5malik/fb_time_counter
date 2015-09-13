d77b4916b321108997f86bdbe24f1eed717eacf8
tsleeve <akash5malik@gmail.com>
2015-09-13 03:41:59 -0700

    refresh with day

--- background.js
+++ background.js
@@ -35,13 +35,18 @@ chrome.tabs.onActivated.addListener(
 					{
 						console.log("more time");
 						var time;
-						chrome.storage.sync.get("time", function(obj){
+						chrome.storage.sync.get(["time", "date"], function(obj){
 							time = obj.time; 
+							var date = obj.date;
 							console.log(obj);
-							if(!time) time = 0;
+							if(!time || time === undefined) time = 0;
 							var date1 = new Date();
+							var todaysDate = date1.getDate();
+							if(!date || date === undefined) date = todaysDate;
+							if(date != todaysDate)
+								time = 0;
 							time += date1.getTime() - timeStarted;
-							chrome.storage.sync.set({"time" : time}, function(){console.log(time/1000);});
+							chrome.storage.sync.set({"time" : time, "date":todaysDate}, function(){console.log(time/1000);});
 							});
 						fb = false;
 					}
@@ -57,13 +62,18 @@ function(request, sender, sendResponse){
 			var time;
 			if(fb)
 				{
-					chrome.storage.sync.get("time", function(obj){
+					chrome.storage.sync.get(["time", "date"], function(obj){
 								time = obj.time; 
-								if(!time) time = 0;
+								var date = obj.date;
+								if(!time || time === undefined) time = 0;
 								var date1 = new Date();
+								var todaysDate = date1.getDate();
+								if(!date || date === undefined) date = todaysDate;
+								if(date != todaysDate)
+									time = 0;
 								time += date1.getTime() - timeStarted;
 								timeStarted = date1.getTime();
-								chrome.storage.sync.set({"time" : time}, function(){console.log(time/1000);});
+								chrome.storage.sync.set({"time" : time, "date":todaysDate}, function(){console.log(time/1000);});
 							});
 				}
 		
--- popup.js
+++ popup.js
@@ -7,11 +7,24 @@ $(document).ready(function(){
 	chrome.storage.onChanged.addListener(function(changes, namespace){
 		var storage_change = changes["time"];
 		time = storage_change.newValue;
-		$("#fb").html(time/1000);
+		var min = time/60000;
+		var sec = (time/1000) % 60;
+		if(Math.floor(min) > 90)
+			$('#fb').css("color", "red");
+		else
+			$('#fb').css("color", "green");
+		$("#fb").html(Math.floor(min) + " minutes, " + Math.floor(sec) + " sec"  );
 });
 	chrome.storage.sync.get("time", function(obj){
 		time = obj.time;
-		$("#fb").html(time/1000);
+		var min = time/60000;
+		var sec = (time/1000) % 60;
+		if(Math.floor(min) >= 90)
+			$('#fb').css("color", "red");
+		else
+			$('#fb').css("color", "green");
+		
+		$("#fb").html( Math.floor(min) + " minutes, " + Math.floor(sec) + " sec" );
 	});
 	
 });
